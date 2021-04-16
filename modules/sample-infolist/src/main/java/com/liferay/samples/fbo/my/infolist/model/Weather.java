386bbaf2e0e567aa63592c44cdcaf8328cb13900
fabian-bouche-liferay <fabian.bouche@liferay.com>
2021-04-16 11:57:16 +0200

    removed useless methods

--- modules/sample-infolist/src/main/java/com/liferay/samples/fbo/my/infolist/model/Weather.java
+++ modules/sample-infolist/src/main/java/com/liferay/samples/fbo/my/infolist/model/Weather.java
@@ -1,11 +1,9 @@
 package com.liferay.samples.fbo.my.infolist.model;
 
 import com.liferay.expando.kernel.model.ExpandoBridge;
-import com.liferay.info.item.InfoItemIdentifier;
 import com.liferay.portal.kernel.model.ClassedModel;
 
 import java.io.Serializable;
-import java.util.Optional;
 
 public class Weather implements ClassedModel {
 
@@ -26,21 +24,6 @@ public class Weather implements ClassedModel {
 	
 	public void setTemperature(int temperature) {
 		this.temperature = temperature;
-	}
-
-	public InfoItemIdentifier getEntryId() {
-		return new InfoItemIdentifier() {
-			
-			@Override
-			public void setVersion(String version) {
-				
-			}
-			
-			@Override
-			public Optional<String> getVersionOptional() {
-				return Optional.empty();
-			}
-		};
 	}
 
 	@Override
