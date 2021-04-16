70f990508f606a3b973f5c9e51d1e90510c43cf5
fabian-bouche-liferay <fabian.bouche@liferay.com>
2021-04-16 11:51:49 +0200

    initial commit

--- modules/sample-infolist/src/main/java/com/liferay/samples/fbo/my/infolist/model/Weather.java
+++ modules/sample-infolist/src/main/java/com/liferay/samples/fbo/my/infolist/model/Weather.java
@@ -1,10 +1,13 @@
 package com.liferay.samples.fbo.my.infolist.model;
 
+import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.info.item.InfoItemIdentifier;
+import com.liferay.portal.kernel.model.ClassedModel;
 
+import java.io.Serializable;
 import java.util.Optional;
 
-public class Weather {
+public class Weather implements ClassedModel {
 
 	private String location;
 	private int temperature;
@@ -38,6 +41,30 @@ public class Weather {
 				return Optional.empty();
 			}
 		};
+	}
+
+	@Override
+	public ExpandoBridge getExpandoBridge() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		return Weather.class;
+	}
+
+	@Override
+	public String getModelClassName() {
+		return Weather.class.getName();
+	}
+
+	@Override
+	public Serializable getPrimaryKeyObj() {
+		return location;
+	}
+
+	@Override
+	public void setPrimaryKeyObj(Serializable primaryKeyObj) {
 	}
 	
 }
--- modules/sample-infolist/src/main/java/com/liferay/samples/fbo/my/infolist/provider/WeatherInfoItemFieldSetProvider.java
+++ modules/sample-infolist/src/main/java/com/liferay/samples/fbo/my/infolist/provider/WeatherInfoItemFieldSetProvider.java
@@ -1,9 +1,21 @@
 package com.liferay.samples.fbo.my.infolist.provider;
 
 import com.liferay.info.field.InfoFieldSet;
+import com.liferay.info.localized.InfoLocalizedValue;
 
-public interface WeatherInfoItemFieldSetProvider {
-	
-	public InfoFieldSet getInfoFieldSet();
+import org.osgi.service.component.annotations.Component;
+
+@Component(immediate = true, service = WeatherInfoItemFieldSetProvider.class)
+public class WeatherInfoItemFieldSetProvider {
+
+	public InfoFieldSet getInfoFieldSet() {
+		return InfoFieldSet.builder()
+				.infoFieldSetEntry(WeatherInfoItemFields.locationInfoField)
+				.infoFieldSetEntry(WeatherInfoItemFields.temperatureInfoField)
+				.labelInfoLocalizedValue(InfoLocalizedValue.localize(getClass(), "weather"))
+				.name("weather")
+				.build();
+	}
 
 }
+
